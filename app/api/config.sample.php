b54f5f12c83b85125b45919137d2985f985c9207
Ismo Vuorinen <ivuorinen@me.com>
2013-11-22 09:29:04 +0200

    Documented the config.sample.php configuration variables and meaning behind them. Also explains the refresh_key usage.

--- app/api/config.sample.php
+++ app/api/config.sample.php
@@ -11,6 +11,24 @@
  * @license  http://www.gnu.org/licenses/agpl.html GNU Affero General Public License
  * @link     https://github.com/Ekokumppanit/Lentolaskuri
  */
+
+/**
+ * Configuration variables for Lentolaskuri
+ *
+ * Database settings
+ * - server       = database server address
+ * - db           = database name
+ * - user         = database username
+ * - password     = database password
+ * - table        = database table to store airport names and locations
+ * - create_table = true or false, should running import.php try to create database table
+ * - refresh_key  = semi-secret key to use to trigger import action: import.php?key=[the key]
+ *
+ * Please change the refresh_key and use it to import airport data to the database.
+ * Use the key as an url parameter: http://example.com/lentolaskuri/api/import.php?key=[the key]
+ *
+ * @var array
+ */
 $config = array(
     'server'       => 'localhost',
     'db'           => 'database',
